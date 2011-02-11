a993d0ee1614bbadb960174d0da36fa2a8e1aebe
gorlum0 <gorlum0@gmail.com>
2011-02-11 22:45:24 +0500

    merge_sort2 with python's extend and slicing assignment

--- sort.py
+++ sort.py
@@ -57,6 +57,29 @@ def merge_sort(A, p, r):
         merge_sort(A, q + 1, r)
         merge(A, p, q, r)
 
+def merge_sort2(A, p, r):
+    """reserved"""
+    def merge(A, p, q, r):
+        B = []
+        i, j = p, q + 1
+        while i <= q and j <= r:
+            if A[i] <= A[j]:
+                B.append(A[i])
+                i += 1
+            else:
+                B.append(A[j])
+                j += 1
+
+        B.extend(A[i: q + 1])
+        B.extend(A[j: r + 1])
+        A[p: p + len(B)] = B
+
+    if p < r:
+        q = (p + r) / 2
+        merge_sort(A, p, q)
+        merge_sort(A, q + 1, r)
+        merge(A, p, q, r)
+
 if __name__ == '__main__':
     A0 = [10, 1, 5, 7, 88, -75]
     print A0; print
@@ -64,3 +87,4 @@ if __name__ == '__main__':
     A = A0[:]; insertion_sort(A); print A
     A = A0[:]; selection_sort(A); print A
     A = A0[:]; merge_sort(A, 0, len(A)-1); print A
+    A = A0[:]; merge_sort2(A, 0, len(A)-1); print A
