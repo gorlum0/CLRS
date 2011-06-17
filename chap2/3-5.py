d04cf98b3b09ffa3fafa232d2a344d6200cda4a3
gorlum0 <gorlum0@gmail.com>
2011-06-17 11:39:15 +0600

    refactoring

--- chap2/3-5.py
+++ chap2/3-5.py
@@ -2,17 +2,17 @@
 """(c) gorlum0 [at] gmail.com"""
 
 def binary_search(A, v):
-    p, r = 0, len(A) - 1
-    q = 0
+    lo, hi = 0, len(A) - 1
 
-    while v != A[q] and p <= r:
-        q = (p + r) // 2
-        if v < A[q]:
-            r = q - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if v == A[mid]:
+            return mid
+        elif v > A[mid]:
+            lo = mid + 1
         else:
-            p = q + 1
-
-    return q if v == A[q] else -1
+            hi = mid - 1
+    return -1
 
 if __name__ == '__main__':
     A = [3, 3, 9, 26, 38, 41, 49, 52, 57, 57]
